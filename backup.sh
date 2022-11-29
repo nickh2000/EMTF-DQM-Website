3961ec8f868392e29b8d4eb3995f9ef486d60b72
root <root@csctiming.cern.ch>
2022-11-29 16:24:22 +0100

    Moved backup.sh to repository

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-semanage fcontext -a -t bin_t '/root/backup.sh'
-restorecon -Fv /root/backup.sh
+sudo semanage fcontext -a -t bin_t '/root/csctiming/backup.sh'
+sudo restorecon -Fv /root/csctiming/backup.sh
 setenforce 0
 export CACHE=/root/keys
 export CACERT=/root/keys/CERN_Root_CA.crt
@@ -8,4 +8,3 @@ export PUBLIC_KEY=/root/keys/usercert.pem
 export PRIVATE_KEY=/root/keys/userkey.key
 source /root/root/bin/thisroot.sh
 /root/csctiming/csctimingenv/bin/gunicorn --chdir=/root/csctiming/ --workers 3 --bind unix:csctiming.sock -m 007 wsgi:app --timeout 0
-
